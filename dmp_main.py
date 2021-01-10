6606760eb9ac532da4e257e467883f5030c7a1fb
cosasas <55668595+cosasas@users.noreply.github.com>
2021-01-10 11:22:15 +0100

    Update dmp_main.py

--- dmp_main.py
+++ dmp_main.py
@@ -13,22 +13,18 @@ def DMP_main(showGraphs, dmp_startingPoint):
     tau = 0.002 * len(demo)
     t = np.arange(0, tau, 0.002)
     demo_p = demo[:, 0:3]
-
-    # TODO: In both canonical_system.py and dmp_position.py you will find some lines missing implementation.
-    # Fix those first.
-
-    N = 50  # TODO: Try changing the number of basis functions to see how it affects the output.
+    
+    N = 50  #Changing the number of basis functions to see how it affects the output.
     dmp = PositionDMP(n_bfs=N, alpha=48.0)
     dmp.train(demo_p, t, tau)
 
-    # TODO: Try setting a different starting point for the dmp:
+    #  different starting point for the dmp
     if dmp_startingPoint != 0:
         dmp.p0 = dmp_startingPoint
-    # TODO: ...or a different goal point:
+    # Goal point:
     #dmp.g0 = [0.2, -0.22, 0.35]
 
-    # TODO: ...or a different time constant:
-    # tau = T
+    #  time constant tau = T
 
     # Generate an output trajectory from the trained DMP
     dmp_p, dmp_dp, dmp_ddp = dmp.rollout(t, tau)
@@ -67,17 +63,5 @@ def DMP_main(showGraphs, dmp_startingPoint):
         ax.set_zlabel('Z')
         ax.legend()
         plt.show()
-
-        # 3D plot the DMP against the original demonstration
-   #     fig2 = plt.figure(2)
-    #    ax = plt.axes(projection='3d')
-     #   ax.plot3D(demo_p[:, 0], demo_p[:, 1], demo_p[:, 2], label='Demonstration')
-     #   ax.plot3D(dmp_p[:, 0], dmp_p[:, 1], dmp_p[:, 2], label='DMP')
-     #   ax.set_xlabel('X')
-     #   ax.set_ylabel('Y')
-     #   ax.set_zlabel('Z')
-     #   ax.legend()
-     #   plt.show()
-
 
     return dmp_p
